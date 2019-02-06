6a6ed965635ea28273c58d3ff784842cb62a24a4
davicillogs <davidgalache7@gmail.com>
2019-02-07 00:30:46 +0100

    Revert "Revert "Revert "ya No Salta despues de morir"""
    This reverts commit efd10a1b44abd7f90c39be400b1348dde14bd34a.

--- Newproyect/Assets/Scripts/PlayerController.cs
+++ Newproyect/Assets/Scripts/PlayerController.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public GameObject game;
     private Animator animator;
     // Start is called before the first frame update
     void Start()
@@ -15,8 +14,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        bool gamePlaying = game.GetComponent<GameController>().gameState == GameState.Playing;
-        if(gamePlaying && (Input.GetKeyDown("up")  || Input.GetMouseButtonDown(0))){
+        if(Input.GetKeyDown("up")  || Input.GetMouseButtonDown(0)){
             UpdateState("PlayerJump");
         }
     }
@@ -30,7 +28,6 @@ public class PlayerController : MonoBehaviour
       void OnTriggerEnter2D(Collider2D other){
         if (other.gameObject.tag == "Enemy"){
             UpdateState("PlayerDie");
-            game.GetComponent<GameController>().gameState = GameState.Ended;
         }
     }
 }
