04cb970c9ea2b5f181d92a0a40051b5afa7fdb3b
davicillogs <davidgalache7@gmail.com>
2019-02-07 01:20:07 +0100

    Muerte Y BUG ANDROIR

--- Newproyect/Assets/Scripts/GameController.cs
+++ Newproyect/Assets/Scripts/GameController.cs
@@ -2,8 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
-public enum GameState {Idle, Playing, Ended};
+public enum GameState {Idle, Playing, Ended, Ready};
 public class GameController : MonoBehaviour
 
 {
@@ -32,10 +33,10 @@ public class GameController : MonoBehaviour
 
     // Update is called once per frame
     void Update() { 
-    
+        bool userAction = Input.GetKeyDown("up")  || Input.GetMouseButtonDown(0);
         //Empieza el juego
 
-        if (gameState == GameState.Idle && (Input.GetKeyDown("up")  || Input.GetMouseButtonDown(0))){
+        if (gameState == GameState.Idle && (userAction)){
             gameState = GameState.Playing;
                 uiIdle.SetActive(false);
                 player.SendMessage("UpdateState","PlayerRun");
@@ -45,11 +46,15 @@ public class GameController : MonoBehaviour
 
         
             //Juego en marcha
-            else if (gameState == GameState.Playing){
+        else if (gameState == GameState.Playing){
                 Parallax();
         }
-        else if (gameState == GameState.Playing){
+        //Juego preparado para reiniciar
+        else if (gameState == GameState.Ready){
                 //TODO
+                if(userAction){
+                    RestartGame();
+                }
         }
         
 
@@ -59,5 +64,8 @@ public class GameController : MonoBehaviour
         float finalSpeed = parallaxSpeed * Time.deltaTime;
         background.uvRect = new Rect(background.uvRect.x + finalSpeed, 0f, 1f, 1f);
         platform.uvRect = new Rect(platform.uvRect.x + finalSpeed, 0f, 1f, 1f);
+    }
+    public void RestartGame(){
+        SceneManager.LoadScene("Principal");
     }
 }
